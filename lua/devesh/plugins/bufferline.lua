c7c816886fe711b5495b57cbda0aac7463bb7d64
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2024-05-31 11:20:33 +0530

    Add bufferline
    - Add lualine

---
+++ lua/devesh/plugins/bufferline.lua
@@ -0,0 +1,17 @@
+return {
+  "seblj/nvim-tabline",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      no_name = '[No Name]',
+      modified_icon = '',
+      close_icon = '',
+      separator = "▌",
+      padding = 3,
+      color_all_icons = false,
+      right_separator = false,
+      show_index = false,
+      show_icon = true,
+    }
+  }
+}
