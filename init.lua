7a689405eb21eb54a7417af7f37d36575696c0b7
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2024-05-31 10:33:23 +0530

    Add lazy.nvim plugin manager
    - Add vim-tmux-navigator, colorscheme, file explorer
    - Add which-key, fzf, greeter, auto-sessions

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
 require("devesh.core")
+require("devesh.lazy")
---
+++ lua/devesh/lazy.lua
@@ -0,0 +1,19 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:name",
+    "https://gitub.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("devesh.plugins", {
+  change_detection = {
+    notify = false,
+  }
+})
+
